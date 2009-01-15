d244660114af8ab3ca8c042bec9a01f746d1366e
Kevin Adler <zeke@zekesdominion.com>
2009-01-15 05:26:48 +0000

    Now renames based on regular expression to not add spaces to files that are already ' - ' formatted

--- space.rb
+++ space.rb
@@ -3,6 +3,6 @@
 movies = Dir['*.{avi,wmv,divx,mpg,mpeg,xvid,mp4,mkv}']
 
 movies.each do |movie|
-	rename = movie.gsub('-', ' - ')
+	rename = movie.gsub(/([^ ])-([^ ])/, '\1 - \2')
 	File::rename(movie, rename)
 end
