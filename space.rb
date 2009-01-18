6699f13ccec260265ce1bf9021940a4306249d9f
Kevin Adler <zeke@zekesdominion.com>
2009-01-18 07:50:57 +0000

    Fixed space so that it correctly renames files and doesn't re-space them. A bug was that matches can't overlap, thus if you have a filename like Show-1-11-Episode.avi, it will match 'w-1' and '1-E', but can't match '1-1' since it is already partially matched by 'w-1'. Thus, we just loop until there is no match.

--- space.rb
+++ space.rb
@@ -3,6 +3,9 @@
 movies = Dir['*.{avi,wmv,divx,mpg,mpeg,xvid,mp4,mkv}']
 
 movies.each do |movie|
-	rename = movie.gsub(/([^ ])-([^ ])/, '\1 - \2')
+	rename = movie
+	while(rename.match(/[^ ]-[^ ]/))
+		rename = rename.gsub(/([^ ])-([^ ])/, '\1 - \2')
+	end
 	File::rename(movie, rename)
 end
