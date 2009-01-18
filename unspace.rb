e686cab4dc6dc3b54127a15c3c0d07f0d412ef1a
Kevin Adler <zeke@zekesdominion.com>
2009-01-18 07:54:48 +0000

    Added unspace.rb to replace ' - ' with '-' in media files for Garret style naming.

---
+++ unspace.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+movies = Dir['*.{avi,wmv,divx,mpg,mpeg,xvid,mp4,mkv}']
+
+movies.each do |movie|
+	rename = movie
+	while(rename.match(/ - /))
+		rename = rename.gsub(/ - /, '-')
+	end
+	File::rename(movie, rename)
+end
