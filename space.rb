9d22d944cfb76db4f50be5080da2c0c34dc7b31c
Kevin Adler <zeke@zekesdominion.com>
2015-02-15 21:40:59 -0600

    Remove obsolete scripts

--- space.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-movies = Dir['*.{avi,wmv,divx,mpg,mpeg,xvid,mp4,mkv}']
-
-movies.each do |movie|
-  rename = movie.gsub(/(?=[^ ])-(?=[^ ])/, ' - ')
-  File::rename(movie, rename)
-end
